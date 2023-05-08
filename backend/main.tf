2966239799524a30c7fcd515236d7b2c3f3cf5e3
unknown <minhdico@gmail.com>
2023-05-08 00:04:55 -0400

    commit

--- backend/main.tf
+++ backend/main.tf
@@ -108,178 +108,178 @@ resource "aws_lambda_function" "updateVisitDetailsToDynamodb" {
   runtime = "python3.8"
 }
 
-# # 4. Configure API Gateway to be integrated with Lambda functions
-# resource "aws_api_gateway_rest_api" "ResumeVisitsCount" {
-#   name        = "ResumeVisitsCount"
-#   description = ""
-# }
+# 4. Configure API Gateway to be integrated with Lambda functions
+resource "aws_api_gateway_rest_api" "ResumeVisitsCount" {
+  name        = "ResumeVisitsCount"
+  description = ""
+}
 
-# # 4.1 Integrate API POST Request with Lambda function updateVisitDetailsToDynamodb
-# resource "aws_api_gateway_resource" "post" {
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   parent_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.root_resource_id}"
-#   path_part   = "post"
-# }
+# 4.1 Integrate API POST Request with Lambda function updateVisitDetailsToDynamodb
+resource "aws_api_gateway_resource" "post" {
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  parent_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.root_resource_id}"
+  path_part   = "post"
+}
 
-# resource "aws_api_gateway_method" "post" {
-#   rest_api_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   resource_id   = "${aws_api_gateway_resource.post.id}"
-#   http_method   = "POST"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "post" {
+  rest_api_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  resource_id   = "${aws_api_gateway_resource.post.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
 
-# resource "aws_api_gateway_integration" "updateVisitDetailsToDynamodb" {
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   resource_id = "${aws_api_gateway_method.post.resource_id}"
-#   http_method = "${aws_api_gateway_method.post.http_method}"
+resource "aws_api_gateway_integration" "updateVisitDetailsToDynamodb" {
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  resource_id = "${aws_api_gateway_method.post.resource_id}"
+  http_method = "${aws_api_gateway_method.post.http_method}"
 
-#   integration_http_method = "POST"
-#   type                    = "AWS"
-#   uri                     = "${aws_lambda_function.updateVisitDetailsToDynamodb.invoke_arn}"
-# }
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "${aws_lambda_function.updateVisitDetailsToDynamodb.invoke_arn}"
+}
 
-# resource "aws_api_gateway_integration_response" "updateVisitDetailsToDynamodb" {
-#     rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#     resource_id = "${aws_api_gateway_method.post.resource_id}"
-#     http_method = aws_api_gateway_method.post.http_method
-#     status_code = 200
+resource "aws_api_gateway_integration_response" "updateVisitDetailsToDynamodb" {
+    rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+    resource_id = "${aws_api_gateway_method.post.resource_id}"
+    http_method = aws_api_gateway_method.post.http_method
+    status_code = 200
 
-#     response_parameters = {
-#         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-#         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-#         "method.response.header.Access-Control-Allow-Origin" = "'*'"
-#     }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
 
-#     depends_on = [
-#         aws_api_gateway_integration.updateVisitDetailsToDynamodb,
-#         aws_api_gateway_method_response.updateVisitDetailsToDynamodb,
-#     ]
-# }
+    depends_on = [
+        aws_api_gateway_integration.updateVisitDetailsToDynamodb,
+        aws_api_gateway_method_response.updateVisitDetailsToDynamodb,
+    ]
+}
 
-# resource "aws_api_gateway_method_response" "updateVisitDetailsToDynamodb" {
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   resource_id = "${aws_api_gateway_method.post.resource_id}"
-#   http_method = aws_api_gateway_method.post.http_method
-#   status_code = 200
+resource "aws_api_gateway_method_response" "updateVisitDetailsToDynamodb" {
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  resource_id = "${aws_api_gateway_method.post.resource_id}"
+  http_method = aws_api_gateway_method.post.http_method
+  status_code = 200
 
-#   response_parameters = {
-#         "method.response.header.Access-Control-Allow-Origin" = true,
-#         "method.response.header.Access-Control-Allow-Methods" = true,
-#         "method.response.header.Access-Control-Allow-Headers" = true,
-#     }
+  response_parameters = {
+        "method.response.header.Access-Control-Allow-Origin" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Headers" = true,
+    }
 
-#   response_models = {
-#     "application/json" = "Empty"
-#   }
+  response_models = {
+    "application/json" = "Empty"
+  }
 
-#   depends_on = [
-#     aws_api_gateway_method.post,
-#   ]
-# }
+  depends_on = [
+    aws_api_gateway_method.post,
+  ]
+}
 
-# resource "aws_api_gateway_deployment" "updateVisitDetailsToDynamodb" {
-#   depends_on = [
-#     aws_api_gateway_integration.updateVisitDetailsToDynamodb,
-#   ]
+resource "aws_api_gateway_deployment" "updateVisitDetailsToDynamodb" {
+  depends_on = [
+    aws_api_gateway_integration.updateVisitDetailsToDynamodb,
+  ]
 
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   stage_name  = "dev"
-# }
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  stage_name  = "dev"
+}
 
-# # 4.2 Integrate API GET Request with Lambda function
-# resource "aws_api_gateway_resource" "get" {
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   parent_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.root_resource_id}"
-#   path_part   = "resource"
-# }
+# 4.2 Integrate API GET Request with Lambda function
+resource "aws_api_gateway_resource" "get" {
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  parent_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.root_resource_id}"
+  path_part   = "resource"
+}
 
-# resource "aws_api_gateway_method" "get" {
-#   rest_api_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   resource_id   = "${aws_api_gateway_resource.get.id}"
-#   http_method   = "GET"
-#   authorization = "NONE"
-# }
+resource "aws_api_gateway_method" "get" {
+  rest_api_id   = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  resource_id   = "${aws_api_gateway_resource.get.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
 
-# resource "aws_api_gateway_integration" "getVisitsCountFromDynamodb" {
-#   rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#   resource_id = "${aws_api_gateway_method.get.resource_id}"
-#   http_method = "${aws_api_gateway_method.get.http_method}"
+resource "aws_api_gateway_integration" "getVisitsCountFromDynamodb" {
+  rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+  resource_id = "${aws_api_gateway_method.get.resource_id}"
+  http_method = "${aws_api_gateway_method.get.http_method}"
 
-#   integration_http_method = "POST"
-#   type                    = "AWS"
-#   uri                     = "${aws_lambda_function.getVisitsCountFromDynamodb.invoke_arn}"
-#   request_templates = {
-#     "application/json" = <<EOF
-# #set($inputRoot = $input.path('$'))
-# {}
-#     EOF
-#   }
-# }
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "${aws_lambda_function.getVisitsCountFromDynamodb.invoke_arn}"
+  request_templates = {
+    "application/json" = <<EOF
+#set($inputRoot = $input.path('$'))
+{}
+    EOF
+  }
+}
 
-# resource "aws_api_gateway_integration_response" "getVisitsCountFromDynamodb" {
-#     rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#     resource_id = "${aws_api_gateway_method.get.resource_id}"
-#     http_method = aws_api_gateway_method.get.http_method
-#     status_code = 200
+resource "aws_api_gateway_integration_response" "getVisitsCountFromDynamodb" {
+    rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+    resource_id = "${aws_api_gateway_method.get.resource_id}"
+    http_method = aws_api_gateway_method.get.http_method
+    status_code = 200
 
-#     response_parameters = {
-#         "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-#         "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-#         "method.response.header.Access-Control-Allow-Origin" = "'*'"
-#     }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+        "method.response.header.Access-Control-Allow-Origin" = "'*'"
+    }
 
-#     depends_on = [
-#         aws_api_gateway_integration.getVisitsCountFromDynamodb,
-#         aws_api_gateway_method_response.getVisitsCountFromDynamodb,
-#     ]
-# }
+    depends_on = [
+        aws_api_gateway_integration.getVisitsCountFromDynamodb,
+        aws_api_gateway_method_response.getVisitsCountFromDynamodb,
+    ]
+}
 
-# resource "aws_api_gateway_method_response" "getVisitsCountFromDynamodb" {
-#     rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#     resource_id = "${aws_api_gateway_method.get.resource_id}"
-#     http_method = aws_api_gateway_method.get.http_method
-#     status_code = 200
+resource "aws_api_gateway_method_response" "getVisitsCountFromDynamodb" {
+    rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+    resource_id = "${aws_api_gateway_method.get.resource_id}"
+    http_method = aws_api_gateway_method.get.http_method
+    status_code = 200
 
-#     response_parameters = {
-#         "method.response.header.Access-Control-Allow-Origin" = true,
-#         "method.response.header.Access-Control-Allow-Methods" = true,
-#         "method.response.header.Access-Control-Allow-Headers" = true,
-#     }
+    response_parameters = {
+        "method.response.header.Access-Control-Allow-Origin" = true,
+        "method.response.header.Access-Control-Allow-Methods" = true,
+        "method.response.header.Access-Control-Allow-Headers" = true,
+    }
 
-#     response_models = {
-#         "application/json" = "Empty"
-#     }
+    response_models = {
+        "application/json" = "Empty"
+    }
 
-#     depends_on = [
-#         aws_api_gateway_method.get,
-#     ]
-# }
+    depends_on = [
+        aws_api_gateway_method.get,
+    ]
+}
 
-# # 5. Deploy API
-# resource "aws_api_gateway_deployment" "getVisitsCountFromDynamodb" {
-#     depends_on = [
-#         aws_api_gateway_integration.getVisitsCountFromDynamodb,
-#     ]
+# 5. Deploy API
+resource "aws_api_gateway_deployment" "getVisitsCountFromDynamodb" {
+    depends_on = [
+        aws_api_gateway_integration.getVisitsCountFromDynamodb,
+    ]
 
-#     rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
-#     stage_name  = "dev"
-# }
+    rest_api_id = "${aws_api_gateway_rest_api.ResumeVisitsCount.id}"
+    stage_name  = "dev"
+}
 
-# # 6. Grant Lambda Invoke permission to API Gateway
-# resource "aws_lambda_permission" "apigw_post" {
-#     statement_id  = "AllowAPIGatewayInvoke1"
-#     action        = "lambda:InvokeFunction"
-#     function_name = "${aws_lambda_function.updateVisitDetailsToDynamodb.function_name}"
-#     principal     = "apigateway.amazonaws.com"
-#     source_arn = "${aws_api_gateway_rest_api.ResumeVisitsCount.execution_arn}/*/POST/${aws_api_gateway_resource.post.path_part}"
-# }
+# 6. Grant Lambda Invoke permission to API Gateway
+resource "aws_lambda_permission" "apigw_post" {
+    statement_id  = "AllowAPIGatewayInvoke1"
+    action        = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.updateVisitDetailsToDynamodb.function_name}"
+    principal     = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.ResumeVisitsCount.execution_arn}/*/POST/${aws_api_gateway_resource.post.path_part}"
+}
 
-# resource "aws_lambda_permission" "apigw_get" {
-#     statement_id  = "AllowAPIGatewayInvoke2"
-#     action        = "lambda:InvokeFunction"
-#     function_name = "${aws_lambda_function.getVisitsCountFromDynamodb.function_name}"
-#     principal     = "apigateway.amazonaws.com"
-#     source_arn = "${aws_api_gateway_rest_api.ResumeVisitsCount.execution_arn}/*/GET/${aws_api_gateway_resource.get.path_part}"
-# }
+resource "aws_lambda_permission" "apigw_get" {
+    statement_id  = "AllowAPIGatewayInvoke2"
+    action        = "lambda:InvokeFunction"
+    function_name = "${aws_lambda_function.getVisitsCountFromDynamodb.function_name}"
+    principal     = "apigateway.amazonaws.com"
+    source_arn = "${aws_api_gateway_rest_api.ResumeVisitsCount.execution_arn}/*/GET/${aws_api_gateway_resource.get.path_part}"
+}
 
 
